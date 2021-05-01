822c985e0f7697d50cb7b027a382a73af7c4534e
AxDEEEEv <slxtty.bxtchlol@gmail.com>
2021-05-01 11:39:47 +0200

    🐄 | First Commit

---
+++ Source/Bot.rb
@@ -0,0 +1,6 @@
+require 'discordrb'
+require 'configatron'
+require_relative 'config.rb'
+bot = Discordrb::Commands::CommandBot.new token: configatron.token, prefix: configatron.prefix
+
+bot.run
---
+++ Source/example.config.rb
@@ -0,0 +1,3 @@
+# change this file too: config.rb
+configatron.token = 'TOKEN HERE'
+configatron.prefix = 'moo '

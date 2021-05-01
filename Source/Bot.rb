7f3eb8c909ed065f6ff998b75a4dc6c5952bd480
AxDEEEEv <slxtty.bxtchlol@gmail.com>
2021-05-01 11:41:03 +0200

    🐄 | hi

--- Source/Bot.rb
+++ Source/Bot.rb
@@ -2,5 +2,5 @@ require 'discordrb'
 require 'configatron'
 require_relative 'config.rb'
 bot = Discordrb::Commands::CommandBot.new token: configatron.token, prefix: configatron.prefix
-
+# hi!
 bot.run
